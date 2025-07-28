64716db2a18bc5c761ac45ebe209ec6d6dd45a7c
João Moreira <joaomoreirati98@gmail.com>
2025-07-27 22:00:28 -0300

    :construction: Adicionando while para exibição do menu

--- sorteador_de_nomes.php
+++ sorteador_de_nomes.php
@@ -2,9 +2,15 @@
 
 $nomes = [];
 
+
+do{
 echo "Menu de opções \n";
 echo "1 - Adicionar nome \n";
 echo "2 - Listar nome \n";
 echo "3 - Sortear nome \n";
 echo "4 - Sair \n";
+
+$opcao = readline("Digite a opção escolhida \n");
+}while($opcao != 4);
+
 ?>
