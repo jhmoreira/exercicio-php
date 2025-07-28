7ed9fb0601fda557e3c222441ba3cfff5f31ff88
João Moreira <joaomoreirati98@gmail.com>
2025-07-27 22:50:05 -0300

    :construction: Implementando opção de sortear nome

--- sorteador_de_nomes.php
+++ sorteador_de_nomes.php
@@ -5,9 +5,9 @@ $nomes = [];
 
 do{
 echo "Menu de opções \n";
-echo "1 - Adicionar nome \n";
-echo "2 - Listar nome \n";
-echo "3 - Sortear nome \n";
+echo "1 - Adicionar nome:  \n";
+if( count($nomes) >=1) echo "2 - Listar nome \n";
+if( count($nomes) >=2) echo "3 - Sortear nome \n";
 echo "4 - Sair \n";
 
 $opcao = readline("Digite a opção escolhida: ");
@@ -20,13 +20,25 @@ switch($opcao){
         echo "\n\n";
         break;
     case 2:
-        echo "Nomes da Lista";
+
+        if( count($nomes) >=1){
+
+        echo "Nomes da Lista: \n";
         foreach($nomes as $nome){
-            echo "-". $nome;
+            echo "- ". $nome;
             echo "\n\n";
-        }
+        };
+};
+        
         break;
     case 3:
+        if( count($nomes) >=2){
+        
+        $indice = array_rand($nomes);
+        echo "Nome sorteado: ". $nomes[$indice];
+        echo "\n\n";
+        unset($nomes[$indice]);
+};
         break;
 
 }
