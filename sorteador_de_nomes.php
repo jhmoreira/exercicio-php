1b1a699cba1172989fa6db496235acf471c8388e
João Moreira <joaomoreirati98@gmail.com>
2025-07-27 22:11:27 -0300

    :construction: Implementando opção de adicionar e listar os nomes

--- sorteador_de_nomes.php
+++ sorteador_de_nomes.php
@@ -10,7 +10,29 @@ echo "2 - Listar nome \n";
 echo "3 - Sortear nome \n";
 echo "4 - Sair \n";
 
-$opcao = readline("Digite a opção escolhida \n");
+$opcao = readline("Digite a opção escolhida: ");
+echo "\n\n";
+
+
+switch($opcao){
+    case 1:
+        $nomes[] = readline("Digite um nome");
+        echo "\n\n";
+        break;
+    case 2:
+        echo "Nomes da Lista";
+        foreach($nomes as $nome){
+            echo "-". $nome;
+            echo "\n\n";
+        }
+        break;
+    case 3:
+        break;
+
+}
+
 }while($opcao != 4);
+
+
 
 ?>
